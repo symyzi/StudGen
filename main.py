28553baf6b4c76584c39a17af8d85d42f1ce3a2e
symyzi <89811910+symyzi@users.noreply.github.com>
2026-01-14 22:25:57 +0300

    Final fix

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import patterns as pat
 
 df = par.load_and_prepare_data("student_success_factors.csv")
 print(df)
-
-print("\nЗапуск анализа топ-20 студентов:\n")
+print("\nЗапуск анализа топ-20 студентов: \n")
 calculate_top20_averages(df)
-print("\nПостроение корреляций:\n")
+print("\nПостроение корреляций: \n")
 cor.plot_correlations(df)
-print("\nПоиск паттернов:\n")
+print("\nПоиск паттернов: \n")
 pat.patterns(df)
 
 
