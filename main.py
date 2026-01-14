9fa81adc097d7832166cd1263c8a602d1eeb0b21
symyzi <89811910+symyzi@users.noreply.github.com>
2026-01-14 22:24:41 +0300

    парсинг

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import patterns as pat
 df = par.load_and_prepare_data("student_success_factors.csv")
 print(df)
 
-print("Запуск анализа топ-20 студентов:")
+print("\nЗапуск анализа топ-20 студентов:\n")
 calculate_top20_averages(df)
+print("\nПостроение корреляций:\n")
 cor.plot_correlations(df)
+print("\nПоиск паттернов:\n")
 pat.patterns(df)
 
 
