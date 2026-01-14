da2b558921db0e9c81a1c6d376b60c5d142bee19
kenfffyk <az09.az09.azrun@mail.ru>
2026-01-14 12:56:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import parse as par
 
+from top20 import calculate_top20_averages
 
 df = par.load_and_prepare_data("student_success_factors.csv")
 print(df)
+
+print("Запуск анализа топ-20 студентов:")
+calculate_top20_averages("student_success_factors.csv")
